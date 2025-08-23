52e3ddf1706b2d0c9de59f14a60f622c968f4e9f
Hodfords <dev@hodfords.com>
2025-08-23 20:08:39 +0700

    feat: init any-guard project

--- lib/guards/first-success.guard.ts
+++ lib/guards/first-success.guard.ts
@@ -17,7 +17,7 @@ export function FirstSuccessGuard(...guards: Type<CanActivate>[]): { new (...arg
 
         async onModuleInit() {
             for (const guard of guards) {
-                const guardInstance = this.moduleRef.get(guard);
+                const guardInstance = this.moduleRef.get(guard, { strict: false });
                 this.guards.push(guardInstance);
             }
         }
